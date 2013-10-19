3a845601773b8bd1bf21f61f3b7819598d816536
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-18 17:26:35 -0700

    Yay new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getsluckynumbers::Application.config.secret_key_base = '30d31f9b26428842098de44b87115a8ddcfed9c363ea94534d2903a3f772604de6f3eb53b5c5bf865d1eab9ff19df315bccfe86e6fc1f79a14e413b1ff2e4b21'
