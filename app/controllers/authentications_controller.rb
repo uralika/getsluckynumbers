d147459f5c37610ac66bf3cf3b2a3551eced1f21
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-28 16:57:25 -0700

    camouflages passwords as user types them

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -14,17 +14,16 @@ class AuthenticationsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:user][:email])
-		if user != nil
+		if user = User.find_by(email: params[:user][:email])
 			# authenticate user
 			if user.authenticate(params[:user][:password])
 				session[:user_id] = user.id
 				redirect_to root_url
-			end
-			else
+			
+			else user 
 				flash.now.alert = "Unable to sign you in. Please try again."
 				render :new
-			
+			end
 		end
 	end
 
